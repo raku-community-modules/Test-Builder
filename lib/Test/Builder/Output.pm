bf47f777c5530a737cc9633f7c1c3b4adf6e8db7
Paul Cochrane <paul@liekut.de>
2016-11-19 13:23:12 +0100

    Update copyright info in source files

--- lib/Test/Builder/Output.pm
+++ lib/Test/Builder/Output.pm
@@ -1,4 +1,5 @@
 # Copyright (C) 2011, Kevin Polulak <kpolulak@gmail.com>.
+# Copyright (C) 2015-2016 The Perl6 Community.
 
 =begin pod
 
@@ -83,6 +84,7 @@ also written by chromatic <chromatic@wgz.org> and Michael G. Schwern
 =head1 COPYRIGHT
 
 Copyright (C) 2011, Kevin Polulak <kpolulak@gmail.com>.
+Copyright (C) 2015-2016 The Perl6 Community.
 
 This program is distributed under the terms of the Artistic License 2.0.
 
